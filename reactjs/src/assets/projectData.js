f21805d5d91ecdc7d2ae4825904bf439515abe59
Adam Harbeck <AHarbeck@student.fullsail.edu>
2022-04-29 13:06:24 -0700

    Project page setup

---
+++ reactjs/src/assets/projectData.js
@@ -0,0 +1,15 @@
+import React from "react";
+import bdHome from '../assets/batter-day/home-page.png';
+import bdHome2 from '../assets/batter-day/home-400x300.png';
+import bdHome3 from '../assets/batter-day/home-500x300.png';
+import bdCategory from '../assets/batter-day/category-page.png';
+import bdProduct from '../assets/batter-day/product-page.png';
+
+const ProjectData = {
+    projName: 'Batter Day',
+    imgs: [[bdHome, bdHome2, bdHome3], bdCategory, bdProduct],
+    summary: "A mock PERN full-stack web application for a bakery. Built usingReact and Node.js with the Express framework, Sequelize ORM, and the Postgres database system.",
+    tech: 'React | Node.js | Express | Sequelize | Postgres | JavaScript | HTML | CSS',
+    fullDesc: 'Blazae',
+}
+export default ProjectData;
