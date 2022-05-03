81d215cbb8de64754be612a19ac4dadb04c8552e
Adam Harbeck <AHarbeck@student.fullsail.edu>
2022-05-03 09:54:12 -0700

    Comments

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import { HashRouter, Route, Routes } from 'react-router-dom'; 
 import Home from './routes/home';
 import Projects from './routes/projects';
 import Contact from './routes/contact';
 import About from './routes/about';
 
+// Originally had BrowserRouter, but that isn't supported on GH pages.
+// Adding basename={process.env.PUBLIC_URL} brings up the content but 404s if refreshed.
+// Changed to HashRouter because it works on GH pages
 function App() {
   return (
     <div>
-      <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+      <HashRouter>
         <Routes>
-          <Route path={''} element={ <Home /> } />
-          <Route path={'projects'} element={ <Projects /> } />
-          <Route path={'contact'} element={ <Contact /> } />
-          <Route path={'about'} element={ <About /> } />
+          <Route path={'/'} element={ <Home /> } />
+          <Route path={'/projects'} element={ <Projects /> } />
+          <Route path={'/contact'} element={ <Contact /> } />
+          <Route path={'/about'} element={ <About /> } />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
